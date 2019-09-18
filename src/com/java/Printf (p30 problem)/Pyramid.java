3406f0eccac55c67782f99e46512441e90194f6b
Mch <827313519@qq.com>
2019-09-18 15:26:54 +0800

    remote Pyramid.java

--- src/com/java/Printf (p30 problem)/Pyramid.java
+++
@@ -1,9 +0,0 @@
-public class Pyramid {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(Syetem.in);
-		
-		System.out.print("请输入金字塔层数：");
-		int i = scan.next	innt();
-		
-	}
-}
