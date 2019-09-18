b606a13edf2305f43a534edc795d410c572258e7
Mch <827313519@qq.com>
2019-09-18 15:20:43 +0800

    Created book p30 problem

---
+++ src/com/java/Printf (p30 problem)/Pyramid.java
@@ -0,0 +1,9 @@
+public class Pyramid {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(Syetem.in);
+		
+		System.out.print("请输入金字塔层数：");
+		int i = scan.next	innt();
+		
+	}
+}
